e7dace0ee7c68d5eea4d6b568d55cafc8bd7fcc8
dragan.manikj <draganmanikj2000@gmail.com>
2024-11-08 14:28:52 +0100

    added few components and styles

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]', //atribute se povikuva <button appButton></button>
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
